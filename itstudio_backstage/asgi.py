9251ed3c5d9d20755e9da1f2c47dd481316747e4
dx2331lxz <3434055686@qq.com>
2023-04-25 22:49:29 +0800

    Initial commit

---
+++ itstudio_backstage/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for itstudio_backstage project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itstudio_backstage.settings')
+
+application = get_asgi_application()
