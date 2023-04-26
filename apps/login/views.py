c7ecb5c8dd594bb942c264f36aa38ecdcc0f05db
dx2331lxz <3434055686@qq.com>
2023-04-27 00:01:41 +0800

    登录，注册和退出登录接口写完

--- apps/login/views.py
+++ apps/login/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from django.contrib import auth
 from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
