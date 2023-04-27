3f2268712256f150b3e894cdd169c04659c22e0a
gugugg <12452250+gugugg@user.noreply.gitee.com>
2023-04-27 17:21:33 +0800

    试试

--- apps/bm/views.py
+++ apps/bm/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib import auth
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User
