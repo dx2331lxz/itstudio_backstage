cf02f05e14d900bbb0bd168abf03ac77264dc3a3
gugugg <12452250+gugugg@user.noreply.gitee.com>
2023-05-10 16:34:28 +0800

    完成报名人员管理和历史表

--- apps/bm/urls.py
+++ apps/bm/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     # 报名人员管理
     path('registrant/', views.RegistrantView.as_view()),
-    path('registrant/delete/', views.RegistantDeleteView.as_view()),
+    path('registrant/delete/', views.RegistrantDeleteView.as_view()),
 
     # 历史表
     path('history/', views.HistoryView.as_view()),
